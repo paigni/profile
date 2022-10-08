cf73db7430c558090bbcd742f052fdce28898d47
paigni <ikontai4@gmail.com>
2022-10-08 17:45:53 +0600

    Рефакторинг кода, добавлена функция на подсчёт количества отработанных лет

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ import os.path
 
 class UserProfile:
 
-    def __init__(self,user_name,user_last_name,birth_day,completed_years,previous_jobs,create_date):
+    def __init__(self, user_name,
+                 user_last_name,
+                 birth_day,
+                 completed_years,
+                 previous_jobs,
+                 create_date,
+                 worked_years):
+
         self.user_name = user_name
         self.user_last_name = user_last_name
         self.birth_day = birth_day
         self.completed_years = completed_years
         self.previous_jobs = previous_jobs
         self.create_date = create_date
-
+        self.worked_years = worked_years
 
     def __str__(self):
         return f'Имя:{self.user_name}\n' \
@@ -22,25 +29,63 @@ class UserProfile:
                f'Места работы: {" ".join(self.previous_jobs)}\n' \
                f'Дата создания:{self.create_date}'
 
+    def __len__(self):
+        """
+        Returns:
+            Количество пользователей
+        """
+        return f'Количество пользователей:{self.read_profiles()}'
+
+    def __int__(self):
+        """
+        Returns:
+            Количество отработанных лет
+        """
+        today = self.now()
+        self.worked_years = int(today) - int(self.create_date)
+        return self.worked_years
 
     def now(self):
         """
+        Returns:
+            Текущую дату
         """
         now = datetime.datetime.now()
         return now.strftime("%d-%m-%Y")
 
+    def user_birth_day(self, day: int, month: int, year: int):
+        """
+        Метод записывает день рождение пользователя
 
-    def completed_years(self,day,month,year):
+        Args:
+            day: День рождения
+            month: Месяц рождения
+            year:Год рождения
+
+        Returns:
+            День рождения
+        """
         self.birth_day = datetime.date(day, month, year).strftime("%d-%m-%Y")
+        return self.birth_day
+
+    def user_completed_years(self):
+        """
+        Returns:
+            Количество полных лет
+        """
         today = self.now()
-        self.completed_years = int(today) - int(self.birth_day)
-
-
+        return self.completed_years == int(today) - int(self.birth_day)
 
     def read_profiles(self):
         """
+        Метод проверяет существование файла
+        Если файл существует, то открывает его
+        Если файла не существует, создаёт его с пустым списком
+
+        Returns:
+            Список профилей
         """
-        check_file = os.path.exists('notes.json')
+        check_file = os.path.exists('profiles.json')
         if not check_file:
             data = []
             self.save_changes(data)
@@ -49,11 +94,6 @@ class UserProfile:
                 data = json.load(write_file)
         return data
 
-
-    def __len__(self):
-        return f'Количество пользователе:{self.read_profiles()}'
-
-
     def save_changes(self, data):
         """
             Метод сохраняет изменения в файле
@@ -61,27 +101,30 @@ class UserProfile:
         with open('profiles.json', 'w') as write_file:
             json.dump(data, write_file)
 
-
-    def action_with_note(self, user_name: str, user_last_name: str, birth_day: str,
-                         completed_years:str, previous_jobs : list,):
+    def create_profiles(self, user_name: str,
+                        user_last_name: str,
+                        previous_jobs: list, ):
         """
+        Метод для создания профиля
+        
+        Args:
+            user_name: День рождения
+            user_last_name: Месяц рождения
+            previous_jobs:
         """
         data = self.read_profiles()
         self.user_name = user_name
         self.user_last_name = user_last_name
-        self.birth_day = birth_day
-        self.completed_years = completed_years
+        self.completed_years = self.user_completed_years()
         self.previous_jobs = previous_jobs
         self.create_date = self.now()
         user_profile = {
-                'user_name': self.user_name,
-                'user_last_name': self.user_last_name,
-                'birth_day': self.birth_day,
-                'completed_years': self.completed_years,
-                'create_date': self.create_date,
-                'previous_jobs': self.previous_jobs
-            }
+            'user_name': self.user_name,
+            'user_last_name': self.user_last_name,
+            'birth_day': self.birth_day,
+            'completed_years': self.completed_years,
+            'create_date': self.create_date,
+            'previous_jobs': self.previous_jobs
+        }
         data.append(user_profile)
         self.save_changes(data)
-
-
